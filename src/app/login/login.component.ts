de1ff3c614e8d428798742918ba67088f474f301
Wilson Daniel Ospina <wdospinal@unal.edu.co>
2017-08-20 23:39:33 -0500

    Refactor login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -19,16 +19,13 @@ export class LoginComponent implements OnInit {
     private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
-    // reset login status
     this.authenticationService.logout();
-
-    // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
   login() {
     this.loading = true;
-    this.authenticationService.login(this.model.username, this.model.password)
+    this.authenticationService.login(this.model.email, this.model.password)
       .subscribe(data => {
         this.router.navigate([this.returnUrl]);
       }, error => {
